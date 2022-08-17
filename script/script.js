06d5702660febcb9bb9445d10b00b6a4fb95e15a
Elmansy <52137036+hamza19944@users.noreply.github.com>
2022-08-17 10:36:09 +0300

    Update script.js
    Update the script

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".typing", {
-    strings:["", "Web Developer", "Full-Stack Developer"],
+    strings:["", "Web Developer", "Frontend Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
