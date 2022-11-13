12abe09f0c60085a25d5f1a6ab641b15f9702cca
hamza19944 <hamza.elmansy94@gmail.com>
2022-11-13 17:12:45 +0300

    js modified

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 let typed = new Typed(".typing", {
-    strings:[" ", "Web Developer", "FrontEnd Developer"],
-    strings:["", "Web Developer", "Frontend Developer"],
+    strings:[" ", "Full Stack Developer", "JavaScript Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
